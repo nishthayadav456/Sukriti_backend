78247e0086814b353d2b16a7c5c5b2266aea1fb1
nishthayadav456 <nishthayadav1122@gmail.com>
2024-10-18 19:42:57 +0530

    first

---
+++ routes/userRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/Usermodel');
+
+// Get all users
+router.get('/', async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+// Add new user 
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete user
+router.delete('/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Update user
+router.put('/:id', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      user.password = password || user.password;
+
+      const updatedUser = await user.save();
+      res.json(updatedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
